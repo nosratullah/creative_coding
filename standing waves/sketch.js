1f8d99cf257c2e3257e4f5e386d299c32df95273
nosratullah <nosratullah.mohammadi@gmail.com>
2023-11-18 20:08:56 +0100

    removing unneccary parts

--- standing waves/sketch.js
+++ standing waves/sketch.js
@@ -4,7 +4,7 @@ var noiseScale = 0.2; // Adjust this value to change the 'smoothness' of the noi
 var radius = 5;
 
 function setup() {
-  createCanvas(1000, 1000);
+  createCanvas(windowWidth, windowHeight);
   
   // noLoop();
   angleMode(DEGREES);
@@ -13,21 +13,14 @@ function setup() {
 }
 
 function draw() {
-  // cluster();
-  grid();
-  
-}
-
-function grid(){
   background('black');
   square_move();
-  
 }
 
 function square_move(){
   
-  for (var i = 0; i < 70; i++) {
-    for (var j = 0; j < 70; j++) {
+  for (var i = 0; i < windowWidth/square_size; i++) {
+    for (var j = 0; j < windowWidth/square_size; j++) {
       noFill();
       // stroke('white');
       push();
@@ -63,42 +56,6 @@ function square_move(){
       
       pop();
 
-    }
-  }
-}
-function circular_move(){
-  for (var i = 0; i < 40; i++) {
-    for (var j = 0; j < 20; j++) {
-      noFill();
-      stroke(100, 100, 100);
-      push();
-      translate(i * 50 + square_size/2, j * 50 + square_size/2);
-      // rect(0, 0, square_size, square_size);
-      fill('white');
-      var angle = millis() / 1000 * 360; // Change the divisor to adjust the speed of the dot
-      // Calculate the x and y positions of the dot
-      var x = square_size/2 * cos(angle + i * 10);
-      var y = square_size/2 * sin(angle + i * 10);
-      // Draw the dot
-      ellipse(x, y, 10, 10);
-      pop();
-      // angle = millis() / 100; // Change the angle with time
-    }
-  }
-}
-function cluster(){
-  r = 20;
-  noStroke();
-  for (var cluster = 0; cluster < 5; cluster++) {
-    var x = random(600);
-    var y = random(1280);
-    fill(random(255), random(255), random(255));
-    ellipse(x, y, r, r);
-    for (var circle = 0; circle < 10; circle++) {
-      var distance_x = random(50);
-      var distance_y = random(50);
-      ellipse(x+distance_x, y+distance_y, r, r);
-      fill(random(255), random(255), random(255), random(255));
     }
   }
 }
